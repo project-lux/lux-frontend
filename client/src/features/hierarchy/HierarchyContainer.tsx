8d4951638a58e54fa17907d7a514c5f206b15b31
kl762 <126-kl762@users.noreply.gitlab.yale.edu>
2024-05-07 16:44:57 -0400

    25 testing adding yarn packages

---
+++ client/src/features/hierarchy/HierarchyContainer.tsx
@@ -0,0 +1,277 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import 'reactflow/dist/style.css'
+import React, { useRef, useState } from 'react'
+import { Button, Col, Row } from 'react-bootstrap'
+import styled from 'styled-components'
+import { isNull } from 'lodash'
+import { useLocation } from 'react-router-dom'
+
+import IEntity from '../../types/data/IEntity'
+import StyledEntityPageSection from '../../styles/shared/EntityPageSection'
+// import ExploreHierarchy from '../common/ExploreHierarchy'
+// import PrimaryButton from '../../styles/shared/PrimaryButton'
+import ILinks from '../../types/data/ILinks'
+import { IHalLink } from '../../types/IHalLink'
+import { useGetSearchRelationshipQuery } from '../../redux/api/ml_api'
+import theme from '../../styles/theme'
+import { useAppSelector } from '../../app/hooks'
+import {
+  getChildEdges,
+  getChildNodes,
+  getDefaultNode,
+  getParentEdges,
+  getParentNodes,
+} from '../../lib/util/hierarchyHelpers'
+import { IHierarchyVisualization } from '../../redux/slices/hierarchyVisualizationSlice'
+import SearchResultsLink from '../relatedLists/SearchResultsLink'
+import { ISearchResults } from '../../types/ISearchResults'
+
+import Hierarchy from './Hierarchy'
+import Node from './Node'
+import ParentCustomNode from './ParentCustomNode'
+import ChildCustomNode from './ChildCustomNode'
+import ListContainer from './ListContainer'
+import Toolbar from './Toolbar'
+import MoreLessButton from './MoreLessButton'
+import BackButton from './BackButton'
+
+interface IProps {
+  entity: IEntity
+  halLink: IHalLink
+  getParentUris: (entity: IEntity) => Array<string>
+}
+
+const getHalLink = (
+  links: ILinks | undefined,
+  halLink: IHalLink,
+): string | null => {
+  if (links === undefined) {
+    return null
+  }
+
+  const { searchTag } = halLink
+  if (links.hasOwnProperty(searchTag)) {
+    return links[searchTag].href
+  }
+
+  return null
+}
+
+const StyledSwitchButton = styled(Button)`
+  background-color: ${theme.color.white} !important;
+  color: ${theme.color.black};
+  border-radius: 10px;
+  border-color: ${theme.color.white} !important;
+  text-decoration: none;
+  padding: 0.5rem;
+
+  &:disabled,
+  &:hover,
+  &:active,
+  &:focus {
+    background-color: ${theme.color.white};
+    color: ${theme.color.black};
+    border-color: ${theme.color.white};
+  }
+`
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const HierarchyContainer: React.FC<IProps> = ({
+  entity,
+  halLink,
+  getParentUris,
+}) => {
+  const [view, setView] = useState<'graph' | 'list'>('graph')
+  const [isFullscreen, setIsFullscreen] = useState<boolean>(false)
+  const defaultHierarchyHeight = '600px'
+  const hierarchyRef = useRef<HTMLDivElement>(null)
+
+  const [displayLength, setDisplayLength] = useState(5)
+
+  const defaultLength = 5
+  const { pathname } = useLocation()
+  let scope: null | string = null
+  if (pathname.includes('concept')) {
+    scope = 'concepts'
+  }
+
+  if (pathname.includes('place')) {
+    scope = 'places'
+  }
+
+  const currentState = useAppSelector(
+    (state) => state.hierarchyVisualization as IHierarchyVisualization,
+  )
+
+  const setFullscreen = (): void => {
+    setIsFullscreen(!isFullscreen)
+    if (hierarchyRef !== null) {
+      const elem = hierarchyRef.current
+      if (isFullscreen) {
+        document.exitFullscreen()
+      } else if (!isFullscreen) {
+        if (elem !== null && elem.requestFullscreen) {
+          elem.requestFullscreen()
+        }
+      }
+    }
+  }
+
+  const currentEntity = isNull(currentState.origin)
+    ? entity
+    : currentState.origin
+  const uri = getHalLink(currentEntity._links, halLink)
+  const parents = getParentUris(currentEntity)
+
+  const skip = uri === null
+  const { data, isSuccess, isError } = useGetSearchRelationshipQuery(
+    {
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      uri: uri!,
+    },
+    {
+      skip,
+    },
+  )
+
+  if (isError) {
+    console.log(
+      'An error occurred retrieving the children of the current entity.',
+    )
+  }
+
+  if ((isSuccess && data) || skip) {
+    // get nodes
+    const currentUuid: string = currentState.origin
+      ? currentState.origin.id!
+      : (entity.id as string)
+
+    const parentNodes = getParentNodes(parents).slice(0, displayLength)
+    const childNodes = data ? getChildNodes(data).slice(0, 5) : []
+    const currentNode = getDefaultNode(currentUuid)
+
+    // get edges
+    const parentEdges = getParentEdges(parentNodes, currentUuid)
+    const childEdges = getChildEdges(childNodes, currentUuid)
+
+    parentNodes.map((parentNode) => {
+      parentNode.data.label = (
+        <ParentCustomNode entityId={parentNode.data.label} />
+      )
+      return null
+    })
+
+    childNodes.map((childNode) => {
+      childNode.data.label = <ChildCustomNode entityId={childNode.data.label} />
+      return null
+    })
+
+    currentNode.data.label = <Node entityId={currentNode.data.label} />
+    // combine nodes and edges
+    const nodes = [currentNode, ...parentNodes, ...childNodes]
+    const edges = [...parentEdges, ...childEdges]
+
+    return (
+      <StyledEntityPageSection
+        className="hierarchyContainer"
+        ref={hierarchyRef}
+      >
+        <Row>
+          <Col xs={8}>
+            <h2 className="mb-0">Explore</h2>
+          </Col>
+          <Col xs={4} className="d-flex justify-content-end">
+            {currentUuid !== entity.id && <BackButton currentEntity={entity} />}
+            <StyledSwitchButton
+              onClick={() => setView(view === 'graph' ? 'list' : 'graph')}
+              role="button"
+              aria-label={`View the hierarchy ${
+                view === 'graph' ? 'list' : 'graph'
+              }`}
+            >
+              <i
+                className={`bi ${
+                  view === 'graph' ? 'bi-list-ul' : 'bi-diagram-3'
+                }`}
+                style={{ fontSize: '1.5rem' }}
+              />
+            </StyledSwitchButton>
+            <StyledSwitchButton
+              onClick={() => setFullscreen()}
+              role="button"
+              aria-label={
+                isFullscreen ? 'Minimize the viewport' : 'Expand to fullscreen'
+              }
+            >
+              <i
+                className={`bi ${
+                  isFullscreen ? 'bi-fullscreen-exit' : 'bi-arrows-fullscreen'
+                }`}
+                style={{ fontSize: '1.5rem' }}
+              />
+            </StyledSwitchButton>
+          </Col>
+        </Row>
+        {view === 'graph' ? (
+          <div
+            style={{
+              height:
+                hierarchyRef.current !== null
+                  ? `${hierarchyRef.current.offsetHeight - 100}px`
+                  : defaultHierarchyHeight,
+            }}
+          >
+            <Hierarchy
+              luxNodes={nodes}
+              luxEdges={edges}
+              currentUuid={currentUuid}
+            >
+              {/* Toolbar associated with parent nodes */}
+              <Toolbar nodeIds={parentNodes.map((node) => node.id)}>
+                <MoreLessButton
+                  parentsLength={parents.length}
+                  defaultDisplayLength={defaultLength}
+                  displayLength={displayLength}
+                  setParentDisplayLength={setDisplayLength}
+                />
+              </Toolbar>
+              {!skip ? (
+                <Toolbar nodeIds={childNodes.map((node) => node.id)}>
+                  <SearchResultsLink
+                    data={(data as ISearchResults) || {}}
+                    eventTitle="Hierarchy Children"
+                    url={data ? data.id : ''}
+                    scope={scope !== null ? scope : 'places'}
+                    additionalLinkText="children"
+                  />
+                </Toolbar>
+              ) : (
+                <React.Fragment />
+              )}
+            </Hierarchy>
+          </div>
+        ) : (
+          <ListContainer
+            displayLength={displayLength}
+            parents={parents}
+            descendents={(data as ISearchResults) || {}}
+            currentEntity={
+              currentState.origin === null ? entity : currentState.origin
+            }
+          >
+            <MoreLessButton
+              parentsLength={parents.length}
+              defaultDisplayLength={defaultLength}
+              displayLength={displayLength}
+              setParentDisplayLength={setDisplayLength}
+            />
+          </ListContainer>
+        )}
+      </StyledEntityPageSection>
+    )
+  }
+
+  return null
+}
+
+export default HierarchyContainer
--- client/src/lib/util/hierarchyHelpers.ts
+++ client/src/lib/util/hierarchyHelpers.ts
@@ -1,7 +1,11 @@
+import { Edge, Node } from 'reactflow'
+
 import IEntity from '../../types/data/IEntity'
 import ILinks from '../../types/data/ILinks'
 import ConceptParser from '../parse/data/ConceptParser'
 import SetParser from '../parse/data/SetParser'
+import { ISearchResults } from '../../types/ISearchResults'
+import EntityParser from '../parse/data/EntityParser'
 
 export const isInHierarchy = (uri: string, ancestors: Array<string>): boolean =>
   ancestors.includes(uri)
@@ -43,6 +47,17 @@ export const getNextConceptUris = (concept: IEntity): Array<string> => {
  * Parses the place entity to determine if the /part_of property exists
  * Used for the breadcrumb hierarchy component on place pages
  * @param {IEntity} entity the current entity to parse
+ * @returns {Array<string>}
+ */
+export const getAllNextConceptUris = (entity: IEntity): Array<string> => {
+  const parser = new ConceptParser(entity)
+  return parser.getBroaderIds()
+}
+
+/**
+ * Parses the place entity to determine if the /part_of property exists
+ * Used for the breadcrumb hierarchy component on place pages
+ * @param {IEntity} entity the current entity to parse
  * @returns {string | null}
  */
 export const getNextPlaceUris = (entity: IEntity): Array<string> => {
@@ -51,6 +66,17 @@ export const getNextPlaceUris = (entity: IEntity): Array<string> => {
     return [partOf[0].id]
   }
   return []
+}
+
+/**
+ * Parses the place entity to determine if the /part_of property exists
+ * Used for the breadcrumb hierarchy component on place pages
+ * @param {IEntity} entity the current entity to parse
+ * @returns {string | null}
+ */
+export const getAllNextPlaceUris = (entity: IEntity): Array<string> => {
+  const parser = new EntityParser(entity)
+  return parser.getPartOf()
 }
 
 /**
@@ -107,3 +133,95 @@ export const getParentData = (
   }
   return data[0]
 }
+
+const DEFAULT_POSITION = { x: 0, y: 0 }
+
+/**
+ * Parses the data and returns nodes for react flow hierarchy
+ * @param {Array<any>} data the current entity to parse
+ * @returns {Array<Node>}
+ */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const getParentNodes = (data: Array<string>): Array<Node> =>
+  data.map((id) => ({
+    id,
+    type: 'parentNode',
+    data: {
+      label: id, // this will be a RecordLink
+    },
+    position: DEFAULT_POSITION,
+  }))
+
+/**
+ * Parses the data and returns nodes for react flow hierarchy
+ * @param {Array<any>} data the current entity to parse
+ * @returns {Array<Node>}
+ */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const getChildNodes = (data: ISearchResults): Array<Node> => {
+  const { orderedItems } = data
+  if (orderedItems.length === 0) {
+    console.log('No children were returned with the provided search criteria.')
+
+    return []
+  }
+
+  return orderedItems.map((obj) => ({
+    id: obj.id,
+    type: 'childNode',
+    data: {
+      label: obj.id, // this will be a RecordLink
+    },
+    position: DEFAULT_POSITION,
+  }))
+}
+
+/**
+ * Parses the nodes to get the correct data
+ * @param {Array<Node>} nodes the current nodes
+ * @param {string} currentUuid the current entity uuid
+ * @returns {Array<Edge>}
+ */
+export const getParentEdges = (
+  nodes: Array<Node>,
+  currentUuid: string,
+): Array<Edge> =>
+  nodes.map((node, ind) => ({
+    id: `e${ind}-${node.id}`, // the uuid of the parent
+    source: node.id, // the source is to the left of the target so the source is the parent
+    targetHandle: 'a', // handle on the target, a will be on the left of current
+    target: currentUuid,
+    // type: 'hierarchyEdge',
+  }))
+
+/**
+ * Parses the nodes to get the correct data
+ * @param {Array<Node>} nodes the current nodes
+ * @param {string} currentUuid the current entity uuid
+ * @returns {Array<Edge>}
+ */
+export const getChildEdges = (
+  nodes: Array<Node>,
+  currentUuid: string,
+): Array<Edge> =>
+  nodes.map((node, ind) => ({
+    id: `e${ind}-${node.id}`, // the uuid of the parent
+    source: currentUuid, // the source is to the left of the target so the source is the parent
+    sourceHandle: 'b', // handle on the target, a will be on the left of current
+    target: node.id,
+    // type: 'hierarchyEdge',
+  }))
+
+/**
+ * Returns the react flow node for the current entity
+ * @param {string} currentUuid the current entity uuid
+ * @returns {Node}
+ */
+export const getDefaultNode = (currentUuid: string): Node => ({
+  id: currentUuid,
+  type: 'originNode',
+  data: {
+    label: currentUuid, // this will be a RecordLink
+  },
+  position: DEFAULT_POSITION,
+})
