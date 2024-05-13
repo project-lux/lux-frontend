944bc2c6a1be1c8f1c884c3df0626b87f3eed420
kl762 <126-kl762@users.noreply.gitlab.yale.edu>
2024-05-13 16:20:47 -0400

    fixed show more and show less functionality

--- client/src/features/common/InternalLink.tsx
+++ client/src/features/common/InternalLink.tsx
@@ -1,41 +1,30 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import styled from 'styled-components'
 
 import { pushSiteImproveEvent } from '../../lib/siteImprove'
 
 interface ILinkParams {
   uri: string
   name?: string
-  className?: string
   linkCategory?: string
   children?: JSX.Element
 }
-
-const StyledLink = styled(Link)`
-  &.active {
-    font-weight: 500;
-    text-decoration: underline;
-  }
-`
 
 /* eslint-disable jsx-a11y/anchor-is-valid */
 const InternalLink: React.FC<ILinkParams> = ({
   uri,
   name,
-  className,
   linkCategory,
   children,
 }) => {
   const eventText = linkCategory !== undefined ? linkCategory : name
 
   return (
-    <StyledLink
+    <Link
       to={uri}
       state={{
         targetName: name,
       }}
-      className={className || ''}
       onClick={() =>
         pushSiteImproveEvent(
           'Internal Link',
@@ -47,7 +36,7 @@ const InternalLink: React.FC<ILinkParams> = ({
     >
       {name}
       {children}
-    </StyledLink>
+    </Link>
   )
 }
 
