2026c21b435454869b3e5656a7a42932c5612436
kl762 <126-kl762@users.noreply.gitlab.yale.edu>
2024-06-11 10:58:33 -0400

    134 added scope to refine label

---
+++ client/src/config/results.ts
@@ -0,0 +1,8 @@
+export const tabToLinkLabel: Record<string, string> = {
+  objects: 'Objects',
+  works: 'Works',
+  people: 'People & Groups',
+  places: 'Places',
+  concepts: 'Concepts',
+  events: 'Events',
+}
