a4bf796b957d4893a1642cbdd0afd1cc5ef0f751
Seong-June Kim <seong-june.kim@yale.edu>
2024-11-20 16:03:31 -0500

    Enable Cognito auth

--- client/src/config/config.ts
+++ client/src/config/config.ts
@@ -20,6 +20,8 @@ class Config {
     options: undefined,
     stopWords: [],
   }
+  // JWT access token from OIDC provider
+  currentAccessToken = ''
 
   constructor() {
     this.env = {
@@ -34,6 +36,10 @@ class Config {
       luxWikidataManifestPrefix: localEnv.luxWikidataManifestPrefix,
       luxFeedbackUrl: localEnv.luxFeedbackUrl,
       bugherdApiKey: localEnv.bugherdApiKey,
+      oidcAuthority: localEnv.oidcAuthority,
+      oidcClientId: localEnv.oidcClientId,
+      oidcRedirectUri: localEnv.oidcRedirectUri,
+      myCollectionsUri: localEnv.myCollectionsUri,
     }
     this.hasLocalEnv =
       localEnv.dataApiBaseUrl !== '' && localEnv.cmsApiBaseUrl !== ''
@@ -53,6 +59,10 @@ class Config {
       luxWikidataManifestPrefix: data.luxWikidataManifestPrefix,
       luxFeedbackUrl: data.luxFeedbackUrl,
       bugherdApiKey: data.bugherdApiKey,
+      oidcAuthority: data.oidcAuthority,
+      oidcClientId: data.oidcClientId,
+      oidcRedirectUri: data.oidcRedirectUri,
+      myCollectionsUri: data.myCollectionsUri,
     }
   }
 
@@ -85,5 +95,8 @@ const config = new Config()
 export const getDataApiBaseUrl = (): string => config.env.dataApiBaseUrl
 export const getFacetsApiBaseUrl = (): string => config.env.facetsApiBaseUrl
 export const getCmsApiBaseUrl = (): string => config.env.cmsApiBaseUrl
+export const getOidcAuthority = (): string => config.env.oidcAuthority
+export const getOidcClientId = (): string => config.env.oidcClientId
+export const getOidcRedirectUri = (): string => config.env.oidcRedirectUri
 
 export default config
--- client/src/redux/api/baseQuery.ts
+++ client/src/redux/api/baseQuery.ts
@@ -2,6 +2,8 @@ import type { BaseQueryFn } from '@reduxjs/toolkit/query/react'
 import axios from 'axios'
 import type { AxiosRequestConfig, AxiosError } from 'axios'
 
+import config from '../../config/config'
+
 export const baseQuery =
   (
     getBaseUrl: () => string,
@@ -16,9 +18,24 @@ export const baseQuery =
     unknown
   > =>
   async ({ url, method, data, params }) => {
+    console.log('BASE')
     try {
       const baseUrl = getBaseUrl()
-      const result = await axios({ url: baseUrl + url, method, data, params })
+      const headers: { [key: string]: string } = {}
+
+      console.log('xx headers:', headers)
+
+      if (config.currentAccessToken) {
+        headers.Authorization = `Bearer ${config.currentAccessToken}`
+      }
+      console.log('headers:', headers)
+      const result = await axios({
+        url: baseUrl + url,
+        method,
+        data,
+        headers,
+        params,
+      })
       return { data: result.data }
     } catch (axiosError) {
       const err = axiosError as AxiosError
