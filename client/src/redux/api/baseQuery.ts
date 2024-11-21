67aba0f28b473c48a506e26af2f01c06904627ab
Seong-June Kim <seong-june.kim@yale.edu>
2024-11-21 16:53:17 -0500

    Verify (parse) JWT token - for debugging

--- client/src/redux/api/baseQuery.ts
+++ client/src/redux/api/baseQuery.ts
@@ -18,7 +18,6 @@ export const baseQuery =
     unknown
   > =>
   async ({ url, method, data, params }) => {
-    console.log('BASE')
     try {
       const baseUrl = getBaseUrl()
       const headers: { [key: string]: string } = {}
