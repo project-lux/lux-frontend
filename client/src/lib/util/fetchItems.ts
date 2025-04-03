67bd53ce16842fb752e7e92429f5ae911c578468
kl762 <126-kl762@users.noreply.gitlab.yale.edu>
2025-04-03 13:22:11 -0400

    refactor of archive hierarchy components

--- client/src/lib/util/fetchItems.ts
+++ client/src/lib/util/fetchItems.ts
@@ -1,12 +1,14 @@
+import { isUndefined } from 'lodash'
+
 /* eslint-disable @typescript-eslint/no-explicit-any */
-export const fetchItems = (uri: string): Promise<any> =>
-  fetch(`${uri}?profile=results`)
+export const fetchItem = (uri: string, profile?: string): Promise<any> =>
+  fetch(`${uri}${isUndefined(profile) ? '' : '?profile=results'}`)
     .then((response) =>
       response.text().then((translatedString) => JSON.parse(translatedString)),
     )
     .catch(() => new Error('An error occurred retrieving the data from the '))
 
-export function getItems(uris: Array<string>): any {
-  const promises = uris.map((uri: string) => fetchItems(uri))
+export function getItems(uris: Array<string>, profile?: string): any {
+  const promises = uris.map((uri: string) => fetchItem(uri, profile))
   return Promise.all(promises).then((result) => ({ data: result }))
 }
