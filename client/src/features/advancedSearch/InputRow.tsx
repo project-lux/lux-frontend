9d5760b4145482fd8d36556c50a53b0bb15e0f18
kl762 <126-kl762@users.noreply.gitlab.yale.edu>
2025-04-25 16:12:38 -0400

    643 changed input row and relationship row styling in advanced search

--- client/src/features/advancedSearch/InputRow.tsx
+++ client/src/features/advancedSearch/InputRow.tsx
@@ -1,28 +1,9 @@
 import React from 'react'
-import { Col } from 'react-bootstrap'
 
-import { useAppDispatch } from '../../app/hooks'
-import { addFieldSelection } from '../../redux/slices/advancedSearchSlice'
-import {
-  isBooleanInput,
-  isDateInput,
-  isRangeInput,
-  isRecordTypeInput,
-  isTextInput,
-} from '../../lib/advancedSearch/advancedSearchParser'
-import AlignedDiv from '../../styles/features/advancedSearch/AlignedDiv'
-import { scopeToAriaLabel } from '../../config/searchTypes'
-import { getParentLabels } from '../../lib/advancedSearch/stateManager'
-import Legend from '../../styles/features/advancedSearch/Legend'
+import StyledInputGroup from '../../styles/features/advancedSearch/InputGroup'
 
-import RangeInput from './RangeInput'
-import AdvancedSearchDropdown from './Dropdown'
-import BooleanInput from './BooleanInput'
-import TextInput from './TextInput'
-import OptionsButton, { INPUT_ROW_TYPE } from './OptionsButton'
 import RemoveButton from './RemoveButton'
-import RecordTypeInput from './RecordTypeInput'
-import DateInput from './DateInput'
+import InputFieldSet from './InputFieldset'
 
 interface IFieldSelectRow {
   stateId: string
@@ -51,99 +32,21 @@ const InputRow: React.FC<IFieldSelectRow> = ({
   state,
   parentStateId,
   nestedLevel,
-}) => {
-  const id = `input-row-${stateId}`
-  const parentLabels = getParentLabels(scope)
-  const labelForAria = parentLabels ? parentLabels[selectedKey] : ''
-
-  const dispatch = useAppDispatch()
-
-  const addOption = (selected: string): void => {
-    dispatch(addFieldSelection({ scope, selected, stateId }))
-  }
-
-  return (
-    <AlignedDiv
-      className="mb-3"
-      data-testid={`${selectedKey}-${stateId}-input-row`}
-    >
-      <Col xs={12}>
-        <fieldset className="d-flex align-content-start">
-          <Legend>input options for {labelForAria}</Legend>
-          <label htmlFor={id} hidden>
-            {selectedKey}
-          </label>
-          <AdvancedSearchDropdown
-            options={parentLabels}
-            handleChange={addOption}
-            className="singleFieldSelection"
-            dropdownHeaderText="Has single field"
-            ariaLabel={`${scopeToAriaLabel[scope]} single field selection`}
-            selected={selectedKey}
-            id={id}
-            scope={scope}
-          />
-          {isBooleanInput(selectedKey) && (
-            <BooleanInput
-              key={selectedKey}
-              label={`Select yes or no for ${labelForAria}`}
-              currentValue={state[selectedKey]}
-              field={selectedKey}
-              stateId={stateId}
-            />
-          )}
-          {isRangeInput(selectedKey) && (
-            <RangeInput
-              label="Enter number"
-              currentValue={state[selectedKey]}
-              field={selectedKey}
-              comp={state._comp}
-              stateId={stateId}
-              ariaLabel={labelForAria}
-            />
-          )}
-          {isRecordTypeInput(selectedKey) && (
-            <RecordTypeInput
-              label={`Select options for ${labelForAria}`}
-              scope={scope}
-              currentValue={state[selectedKey]}
-              field={selectedKey}
-              stateId={stateId}
-            />
-          )}
-          {isDateInput(selectedKey) && (
-            <DateInput
-              label={`Select options for ${labelForAria}`}
-              comp={state._comp}
-              currentValue={state[selectedKey]}
-              field={selectedKey}
-              stateId={stateId}
-              ariaLabel={labelForAria}
-            />
-          )}
-          {isTextInput(selectedKey) && (
-            <TextInput
-              label="Enter a text value"
-              currentValue={state[selectedKey]}
-              field={selectedKey}
-              stateId={stateId}
-              scope={scope}
-              // eslint-disable-next-line jsx-a11y/no-autofocus
-              autoFocus
-            />
-          )}
-          <OptionsButton
-            state={state}
-            stateId={stateId}
-            ariaLabel={labelForAria}
-            nestedLevel={nestedLevel}
-            rowType={INPUT_ROW_TYPE}
-          />
-          <RemoveButton stateId={stateId} parentStateId={parentStateId} />
-        </fieldset>
-      </Col>
-    </AlignedDiv>
-  )
-}
+}) => (
+  <StyledInputGroup className="col-12 px-0 w-100 mb-3">
+    <span className="w-100 d-flex ps-2">
+      <InputFieldSet
+        stateId={stateId}
+        scope={scope}
+        selectedKey={selectedKey}
+        state={state}
+        parentStateId={stateId}
+        nestedLevel={nestedLevel}
+        className="py-2"
+      />
+      <RemoveButton stateId={stateId} parentStateId={parentStateId} />
+    </span>
+  </StyledInputGroup>
+)
 
 export default InputRow
--- client/src/features/advancedSearch/RemoveButton.tsx
+++ client/src/features/advancedSearch/RemoveButton.tsx
@@ -1,5 +1,5 @@
 import React, { MouseEvent } from 'react'
-import { Button } from 'react-bootstrap'
+import { Button, InputGroup } from 'react-bootstrap'
 import styled from 'styled-components'
 
 import { useAppDispatch } from '../../app/hooks'
@@ -77,20 +77,24 @@ const RemoveButton: React.FC<IRemoveButton> = ({
   }
 
   return (
-    <StyledButton
-      id={`remove-${stateId}`}
-      aria-label="remove row"
-      data-testid={`${stateId}-${parentStateId}-remove-row-button${
-        childInd ? `-${childInd}` : ''
-      }`}
-      onClick={() => removeOption()}
-      onMouseEnter={(e: MouseEvent<HTMLButtonElement>) =>
-        handleOnMouseEnter(e, 'remove')
-      }
-      onMouseLeave={(e: MouseEvent<HTMLButtonElement>) => handleOnMouseLeave(e)}
-    >
-      <i className="bi bi-trash3 fs-4" />
-    </StyledButton>
+    <InputGroup.Text className="removeButtonInputGroup">
+      <StyledButton
+        id={`remove-${stateId}`}
+        aria-label="remove row"
+        data-testid={`${stateId}-${parentStateId}-remove-row-button${
+          childInd ? `-${childInd}` : ''
+        }`}
+        onClick={() => removeOption()}
+        onMouseEnter={(e: MouseEvent<HTMLButtonElement>) =>
+          handleOnMouseEnter(e, 'remove')
+        }
+        onMouseLeave={(e: MouseEvent<HTMLButtonElement>) =>
+          handleOnMouseLeave(e)
+        }
+      >
+        <i className="bi bi-trash3 fs-4" />
+      </StyledButton>
+    </InputGroup.Text>
   )
 }
 
