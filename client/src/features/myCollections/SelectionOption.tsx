6dadfb98da943a20112ea33f1ac62fc4d482127a
kl762 <126-kl762@users.noreply.gitlab.yale.edu>
2025-06-23 11:04:04 -0400

    added final modal component

--- client/src/features/myCollections/SelectionOption.tsx
+++ client/src/features/myCollections/SelectionOption.tsx
@@ -1,22 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 
 import theme from '../../styles/theme'
 import ApiText from '../common/ApiText'
 
-const StyledDiv = styled.div`
+interface IStyledDivProps {
+  selected: boolean
+}
+
+const StyledDiv = styled.div<IStyledDivProps>`
+  color: ${theme.color.trueBlack};
+  background-color: ${(props) =>
+    props.selected ? theme.color.lightBabyBlue : 'none'};
+
   &:hover {
     background-color: ${theme.color.lightBabyBlue};
-    color: ${theme.color.trueBlack};
   }
 `
 
 const SelectionOption: React.FC<{
-  collection: string
-}> = ({ collection }) => (
-  <StyledDiv onClick={() => alert('it works')} data-testid="collection-select">
-    {ApiText(collection)}
-  </StyledDiv>
-)
+  record: string
+}> = ({ record }) => {
+  const [isSelected, setIsSelected] = useState<boolean>(true)
+
+  return (
+    <StyledDiv
+      className="py-1 px-2"
+      data-testid="record-selection-div"
+      selected={isSelected}
+    >
+      <input
+        className="checkbox d-inline mt-0 selectAllResultsCheckbox"
+        type="checkbox"
+        id="select-all-checkbox"
+        onChange={() => setIsSelected(!isSelected)}
+        checked={isSelected}
+      />
+      <label className="form-check-label ms-2" htmlFor="select-all-checkbox">
+        {ApiText(record)}
+      </label>
+    </StyledDiv>
+  )
+}
 
 export default SelectionOption
