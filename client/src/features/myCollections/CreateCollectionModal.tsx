b759c4b0311ed2a554adf12cf3063eeed0ed2797
kl762 <126-kl762@users.noreply.gitlab.yale.edu>
2025-06-24 11:28:24 -0400

    added form control for creating a collection

--- client/src/features/myCollections/CreateCollectionModal.tsx
+++ client/src/features/myCollections/CreateCollectionModal.tsx
@@ -1,4 +1,4 @@
-import React, { FormEventHandler } from 'react'
+import React, { ChangeEvent, useState } from 'react'
 import { Col, Form, Modal, Row } from 'react-bootstrap'
 
 import PrimaryButton from '../../styles/shared/PrimaryButton'
@@ -24,11 +24,14 @@ const CreateCollectionModal: React.FC<IMyCollectionsModal> = ({
   const year = date.getUTCFullYear()
   const time = date.toLocaleTimeString()
   const timestamp = `[${month}/${day}/${year} at ${time}]`
+  const placeholderName = `Collection ${timestamp}`
+  const [name, setName] = useState<string>(placeholderName)
+  const [classification, setClassification] = useState<string>('Primary Name')
+  const [language, setLanguage] = useState<string>('English')
+  const [isDefault, setIsDefault] = useState<boolean>(false)
 
-  const handleSave = (
-    event: React.FormEvent<HTMLFormElement>,
-  ): void => {
-    console.log(event)
+  const handleSave = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault()
     onClose()
     // push to ML
   }
@@ -55,7 +58,10 @@ const CreateCollectionModal: React.FC<IMyCollectionsModal> = ({
               <Form.Label>Name</Form.Label>
               <Form.Control
                 type="text"
-                placeholder={`Collection ${timestamp}`}
+                placeholder={placeholderName}
+                value={name}
+                // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                onChange={(e: ChangeEvent<any>) => setName(e.target.value)}
               />
             </Form.Group>
           </Row>
@@ -63,29 +69,49 @@ const CreateCollectionModal: React.FC<IMyCollectionsModal> = ({
           <Row className="mb-3">
             <Form.Group as={Col} controlId="formGridCity">
               <Form.Label>Classification (required)</Form.Label>
-              <Form.Select defaultValue="Choose..." required>
+              <Form.Select
+                defaultValue="Choose..."
+                value={classification}
+                onChange={(e: ChangeEvent<HTMLSelectElement>) =>
+                  setClassification(e.target.value)
+                }
+                required
+              >
                 <option>Primary Name</option>
+                <option>Secondary Name</option>
                 <option>...</option>
               </Form.Select>
             </Form.Group>
 
             <Form.Group as={Col} controlId="formGridState">
               <Form.Label>Language of Name (optional)</Form.Label>
-              <Form.Select defaultValue="Choose...">
+              <Form.Select
+                defaultValue="Choose..."
+                value={language}
+                onChange={(e: ChangeEvent<HTMLSelectElement>) =>
+                  setLanguage(e.target.value)
+                }
+              >
                 <option>English</option>
+                <option>Spanish</option>
+                <option>Latin</option>
                 <option>...</option>
               </Form.Select>
             </Form.Group>
           </Row>
 
-          <Form.Group className="mb-3" id="formGridCheckbox">
-            <Form.Check label="Set as default collection" />
+          <Form.Group id="formGridCheckbox">
+            <Form.Check
+              inline
+              type="checkbox"
+              checked={isDefault}
+              onChange={() => setIsDefault(!isDefault)}
+              label="Set as default collection"
+            />
           </Form.Group>
           <Row>
             <Col className="d-flex justify-content-end">
-              <PrimaryButton type="submit">
-                Save
-              </PrimaryButton>
+              <PrimaryButton type="submit">Save</PrimaryButton>
             </Col>
           </Row>
         </Form>
