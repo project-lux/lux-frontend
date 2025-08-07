cc4965342a9f9d3f12cdf6d699c3f175bcbb4ea8
kl762 <126-kl762@users.noreply.gitlab.yale.edu>
2025-08-07 09:15:09 -0400

    657 changed edit names form

--- client/src/features/myCollections/EditNamesForm.tsx
+++ client/src/features/myCollections/EditNamesForm.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React, { ChangeEvent, useState } from 'react'
 import { Col, Form, Row } from 'react-bootstrap'
 
 import SecondaryButton from '../../styles/shared/SecondaryButton'
@@ -6,8 +7,12 @@ import IMyCollection from '../../types/data/IMyCollection'
 import MyCollectionParser from '../../lib/parse/data/MyCollectionParser'
 import theme from '../../styles/theme'
 import PrimaryButton from '../../styles/shared/PrimaryButton'
+import INames from '../../types/myCollections/INames'
+import { commonClassifications } from '../../config/myCollections/classifications'
+import { commonLanguages } from '../../config/myCollections/languages'
+import StyledDeleteButton from '../../styles/features/myCollections/DeleteButton'
 
-import EditNameFormElements from './EditNameFormElements'
+import MultiSelectDropdown from './MultiSelectDropdown'
 
 interface IMyCollectionsModal {
   data: IMyCollection
@@ -21,39 +26,148 @@ interface IMyCollectionsModal {
  */
 const EditNamesForm: React.FC<IMyCollectionsModal> = ({ data, onFormSave }) => {
   const myCollection = new MyCollectionParser(data)
-  const namesData = myCollection.getNamesForEditing()
-  const [namesToEdit, setNamesToEdit] = useState<
-    Array<{
-      name: string
-      languages: Array<string>
-      classifications: Array<string>
-    }>
-  >(namesData)
+  const collectionNames = myCollection.getNamesForEditing()
+  const defaultNamesData: INames = {
+    name: '',
+    classifications: [],
+    languages: [],
+  }
+  const [names, setNames] = useState<Array<INames>>(
+    collectionNames.length > 0 ? collectionNames : [defaultNamesData],
+  )
 
-  const handleAddNewName = (): void => {
-    setNamesToEdit([
-      ...namesToEdit,
-      { name: '', classifications: [], languages: [] },
-    ])
+  const handleNameInputChange = (
+    e: ChangeEvent<any>,
+    indexOfNameToEdit: number,
+  ): void => {
+    const { value } = e.target
+    const newIdArray = [...names]
+    newIdArray[indexOfNameToEdit].name = value
+    setNames(newIdArray)
   }
 
-  const handleDeleteName = (indexOfNameToDelete: number): void => {
-    setNamesToEdit(namesToEdit.filter((n, i) => i !== indexOfNameToDelete))
+  const handleSelectNameClassification = (
+    e: ChangeEvent<any>,
+    indexOfNameToEdit: number,
+  ): void => {
+    const { value } = e.target
+    const newNoteArray = [...names]
+    const noteToEdit = newNoteArray[indexOfNameToEdit]
+    if (noteToEdit.hasOwnProperty('classifications')) {
+      const { classifications } = noteToEdit
+      if (noteToEdit.classifications?.includes(value)) {
+        const indexOfClassificationToRemove = classifications?.indexOf(
+          value,
+        ) as number
+        classifications?.splice(indexOfClassificationToRemove, 1)
+      } else {
+        classifications?.push(value)
+      }
+    } else {
+      noteToEdit.classifications = [value]
+    }
+    newNoteArray[indexOfNameToEdit] = noteToEdit
+    setNames(newNoteArray)
+  }
+
+  const handleSelectNameLanguage = (
+    e: ChangeEvent<any>,
+    indexOfNameToEdit: number,
+  ): void => {
+    const { value } = e.target
+    const newNoteArray = [...names]
+    const noteToEdit = newNoteArray[indexOfNameToEdit]
+    if (noteToEdit.hasOwnProperty('languages')) {
+      const { languages } = noteToEdit
+      if (noteToEdit.languages?.includes(value)) {
+        const indexOfClassificationToRemove = languages?.indexOf(
+          value,
+        ) as number
+        languages?.splice(indexOfClassificationToRemove, 1)
+      } else {
+        languages?.push(value)
+      }
+    } else {
+      noteToEdit.languages = [value]
+    }
+    newNoteArray[indexOfNameToEdit] = noteToEdit
+    setNames(newNoteArray)
+  }
+
+  const handleAddNewName = (): void => {
+    setNames([...names, defaultNamesData])
+  }
+
+  const handleRemoveName = (indexOfNameToDelete: number): void => {
+    setNames(names.filter((n, i) => i !== indexOfNameToDelete))
   }
 
   return (
     <Form onSubmit={onFormSave}>
-      {namesToEdit.map((d, ind) => {
+      {names.map((d, ind) => {
         const { name, languages, classifications } = d
         return (
-          <EditNameFormElements
-            index={ind + 1}
-            name={name}
-            languages={languages}
-            classifications={classifications}
-            namesLength={namesToEdit.length}
-            handleDeleteName={handleDeleteName}
-          />
+          <React.Fragment>
+            <Row className="mb-3">
+              <Form.Group as={Col} controlId="formGridName">
+                <Row>
+                  <Col className="d-flex align-items-center">
+                    <Form.Label className="fs-5 mb-0">
+                      <strong>Name {ind + 1}</strong>
+                    </Form.Label>
+                  </Col>
+                  {names.length > 1 && (
+                    <Col className="d-flex justify-content-end">
+                      <StyledDeleteButton
+                        aria-label={`Delete Name ${ind}`}
+                        onClick={() => handleRemoveName(ind)}
+                        className="mb-2"
+                      >
+                        <i className="bi bi-trash3 fs-4" />
+                      </StyledDeleteButton>
+                    </Col>
+                  )}
+                </Row>
+                <Form.Control
+                  type="text"
+                  placeholder={name !== '' ? name : 'Enter name...'}
+                  value={name}
+                  onChange={(e: ChangeEvent<any>) =>
+                    handleNameInputChange(e.target.value, ind)
+                  }
+                />
+              </Form.Group>
+            </Row>
+
+            <Row className="mb-3">
+              <Form.Group as={Col} controlId="nameClassificationFormGroup">
+                <Form.Label>Classification (required)</Form.Label>
+                <Form.Control
+                  as={MultiSelectDropdown}
+                  options={commonClassifications}
+                  selectedOptions={classifications}
+                  indexOfData={ind}
+                  ariaLabel="Select one or more classifications for the name"
+                  className="editNameClassificationDropdownButton"
+                  onCheck={handleSelectNameClassification}
+                  required
+                />
+              </Form.Group>
+
+              <Form.Group as={Col} controlId="formGridState">
+                <Form.Label>Language of Name (optional)</Form.Label>
+                <Form.Control
+                  as={MultiSelectDropdown}
+                  options={commonLanguages}
+                  selectedOptions={languages}
+                  indexOfData={ind}
+                  ariaLabel="Select one or more languages for the name"
+                  className="editNameLanguageDropdownButton"
+                  onCheck={handleSelectNameLanguage}
+                />
+              </Form.Group>
+            </Row>
+          </React.Fragment>
         )
       })}
       <Row>
