2deedb4b7fcd999a2b0a8e7a91572cb9d075b0d7
kl762 <126-kl762@users.noreply.gitlab.yale.edu>
2026-02-20 11:53:55 -0500

    518 added additional relationships to Event page timelines

--- client/src/config/eventSearchTags.ts
+++ client/src/config/eventSearchTags.ts
@@ -101,9 +101,29 @@ export const relatedAccordions: IHalLinks = {
 
 // Used for rendering timelines
 export const timelines: IHalLinks = {
-  setAboutDate: {
-    searchTag: 'lux:eventSetAboutTime',
-    tab: 'collections',
+  itemIncludedDate: {
+    searchTag: 'lux:eventItemIncludedTime',
+    tab: 'objects',
     jsonSearchTerm: 'publishedDate,createdDate',
+  },
+  workCreationOrPublicationDate: {
+    searchTag: 'lux:eventWorksAboutTime',
+    tab: 'works',
+    jsonSearchTerm: 'publishedDate,createdDate',
+  },
+  workCausedBy: {
+    searchTag: 'lux:eventWorksCausedByTime',
+    tab: 'works',
+    jsonSearchTerm: 'causedCreationOfDate', // needs to be created
+  },
+  setCausedBy: {
+    searchTag: 'lux:eventSetsCausedByTime',
+    tab: 'collections',
+    jsonSearchTerm: 'causedCreationOfDate', // needs to be created
+  },
+  setAboutDate: {
+    searchTag: 'lux:eventSetsAboutTime',
+    tab: 'collections',
+    jsonSearchTerm: 'publishedDate,createdDate', // this should be correct
   },
 }
