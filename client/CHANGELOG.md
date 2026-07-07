db98974f83a4d4ce9d64528755f3085364acd1b1
kl762 <126-kl762@users.noreply.gitlab.yale.edu>
2026-07-07 12:52:47 -0400

    985 maintenance cycle for 7/20/26

--- client/CHANGELOG.md
+++ client/CHANGELOG.md
@@ -8,6 +8,8 @@ All notable changes to this project will be documented in this file.
 
 ### Changed
 
+- Updated packages ([#985](https://github.com/project-lux/lux-frontend/issues/985)).
+
 ### Fixed
 
 ### Removed
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import viteTsconfigPaths from 'vite-tsconfig-paths'
 import eslint from 'vite-plugin-eslint2'
 
 export default defineConfig(({ command, mode }) => {
@@ -8,12 +7,19 @@ export default defineConfig(({ command, mode }) => {
   // depending on your application, base can also be "/"
   return {
     base: '/',
-    plugins: [react(), viteTsconfigPaths(), eslint()],
+    plugins: [react(), eslint({
+      eslintPath: 'eslint',
+      cache: false,
+      include: ['src/**/*.js', 'src/**/*.jsx', 'src/**/*.ts', 'src/**/*.tsx'],
+    })],
     // vite config
     define: {
       __APP_ENV__: JSON.stringify(env.APP_ENV),
     },
     envPrefix: 'REACT_',
+    resolve: {
+      tsconfigPaths: true
+    },
     server: {
       // this ensures that the browser opens upon server start
       open: true,
